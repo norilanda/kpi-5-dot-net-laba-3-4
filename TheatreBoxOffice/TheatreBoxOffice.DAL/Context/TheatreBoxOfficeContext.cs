c814cb02f2b1281d51c5fccdefe2051279a4f55f
Lidia Makarchuk <makarchuk.lidiia@gmail.com>
2023-12-05 21:08:16 +0200

    Add dbContext

---
+++ TheatreBoxOffice/TheatreBoxOffice.DAL/Context/TheatreBoxOfficeContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace TheatreBoxOffice.DAL.Context;
+
+public class TheatreBoxOfficeContext : IdentityDbContext<IdentityUser>
+{
+    public TheatreBoxOfficeContext(DbContextOptions<TheatreBoxOfficeContext> options) : base(options)
+    {
+        
+    }
+}
